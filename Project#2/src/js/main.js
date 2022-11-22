8bb83ef4d715b989dd2d1db3f6d75f19bc66a168
Sviatoslav <blackdbf@gmail.com>
2022-11-22 13:10:14 +0200

    add accordion, burger menu and picturesize on mouse

--- Project#2/src/js/main.js
+++ Project#2/src/js/main.js
@@ -6,6 +6,9 @@ import checkTextInputs from "./modules/checkTextInputs";
 import showMoreStyles from "./modules/showMoreStyles";
 import calc from "./modules/calc";
 import filter from "./modules/filter";
+import pictureSize from "./modules/pictureSize";
+import accordion from "./modules/accordion";
+import burger from "./modules/burger";
 
 window.addEventListener("DOMContentLoaded", () => {
     "use strict";
@@ -20,4 +23,8 @@ window.addEventListener("DOMContentLoaded", () => {
     showMoreStyles(".button-styles", "#styles .row");
     calc("#size", "#material", "#options", ".promocode", ".calc-price");
     filter();
+    pictureSize(".sizes-block");
+    // accordion(".accordion-heading", ".accordion-block");
+    accordion(".accordion-heading");
+    burger(".burger-menu", ".burger");
 });
