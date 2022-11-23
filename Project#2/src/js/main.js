4f32619eb6a18f391e484f294a4e320d82318bb3
Sviatoslav <blackdbf@gmail.com>
2022-11-23 11:23:19 +0200

    add function dragNdrop

--- Project#2/src/js/main.js
+++ Project#2/src/js/main.js
@@ -10,6 +10,7 @@ import pictureSize from "./modules/pictureSize";
 import accordion from "./modules/accordion";
 import burger from "./modules/burger";
 import scrolling from "./modules/scrolling";
+import dragNdrop from "./modules/dragNdrop";
 
 window.addEventListener("DOMContentLoaded", () => {
     "use strict";
@@ -29,4 +30,5 @@ window.addEventListener("DOMContentLoaded", () => {
     accordion(".accordion-heading");
     burger(".burger-menu", ".burger");
     scrolling(".pageup");
+    dragNdrop();
 });
