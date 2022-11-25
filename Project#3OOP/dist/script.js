eba3da39a49c3f0d1cf23bc4e4f4859a76cc453e
Sviatoslav <blackdbf@gmail.com>
2022-11-25 11:36:04 +0200

    added animation for block hanson in third page

--- Project#3OOP/dist/script.js
+++ Project#3OOP/dist/script.js
@@ -974,21 +974,38 @@ function () {
   _createClass(Slider, [{
     key: "showSlides",
     value: function showSlides(n) {
+      var _this = this;
+
       if (n > this.slides.length) {
         this.slideIndex = 1;
       }
 
       if (n < 1) {
         this.slideIndex = this.slides.length;
-      }
+      } // console.log(this.slideIndex);
 
-      console.log(this.slideIndex);
+
+      try {
+        this.hanson.style.opacity = '0';
+
+        if (n === 3) {
+          this.hanson.classList.add('animated');
+          setTimeout(function () {
+            _this.hanson.style.opacity = '1';
+
+            _this.hanson.classList.add('slideInUp');
+          }, 3000);
+        } else {
+          this.hanson.classList.remove('slideInUp');
+        }
+      } catch (err) {}
+
       this.slides.forEach(function (slide) {
         slide.style.display = 'none';
-        slide.classList.remove("animated", "slideInDown");
+        slide.classList.remove("animated", "slideInUp");
       });
       this.slides[this.slideIndex - 1].style.display = 'block';
-      this.slides[this.slideIndex - 1].classList.add("animated", "slideInDown");
+      this.slides[this.slideIndex - 1].classList.add("animated", "slideInUp");
     }
   }, {
     key: "plusSlides",
@@ -998,17 +1015,21 @@ function () {
   }, {
     key: "render",
     value: function render() {
-      var _this = this;
+      var _this2 = this;
+
+      try {
+        this.hanson = document.querySelector(".hanson");
+      } catch (err) {}
 
       this.btns.forEach(function (btn) {
         btn.addEventListener('click', function () {
-          _this.plusSlides(1);
+          _this2.plusSlides(1);
         });
         btn.parentNode.previousElementSibling.addEventListener('click', function (e) {
           e.preventDefault();
-          _this.slideIndex = 1;
+          _this2.slideIndex = 1;
 
-          _this.showSlides(_this.slideIndex);
+          _this2.showSlides(_this2.slideIndex);
         });
       });
       this.showSlides(this.slideIndex);
--- Project#3OOP/src/js/modules/slider.js
+++ Project#3OOP/src/js/modules/slider.js
@@ -13,15 +13,29 @@ export default class Slider {
         if (n < 1) {
             this.slideIndex = this.slides.length;
         }
-        console.log(this.slideIndex);
+        // console.log(this.slideIndex);
+
+        try {
+            this.hanson.style.opacity = '0';
+
+            if (n === 3) {
+                this.hanson.classList.add('animated');
+                setTimeout(() => {
+                    this.hanson.style.opacity = '1';
+                    this.hanson.classList.add('slideInUp');
+                }, 3000);
+            } else {
+                this.hanson.classList.remove('slideInUp');
+            }
+        } catch (err) { }
 
         this.slides.forEach(slide => {
             slide.style.display = 'none';
-            slide.classList.remove("animated", "slideInDown");
+            slide.classList.remove("animated", "slideInUp");
         });
 
         this.slides[this.slideIndex - 1].style.display = 'block';
-        this.slides[this.slideIndex - 1].classList.add("animated","slideInDown");
+        this.slides[this.slideIndex - 1].classList.add("animated", "slideInUp");
     }
 
     plusSlides(n) {
@@ -29,6 +43,10 @@ export default class Slider {
     }
 
     render() {
+        try {
+            this.hanson = document.querySelector(".hanson");
+        } catch (err) {  }
+        
         this.btns.forEach(btn => {
             btn.addEventListener('click', () => {
                 this.plusSlides(1);
