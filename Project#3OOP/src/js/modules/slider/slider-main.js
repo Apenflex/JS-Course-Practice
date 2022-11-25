ccfb03e7d3353c6caead434b3673dc1d0b45e4ba
Sviatoslav <blackdbf@gmail.com>
2022-11-25 13:20:43 +0200

    added mainSlider and done destructorization for next sliders

---
+++ Project#3OOP/src/js/modules/slider/slider-main.js
@@ -0,0 +1,66 @@
+import Slider from './slider';
+
+export default class MainSlider extends Slider {
+    constructor(page, btns) {
+        super(page, btns);
+    }
+
+    showSlides(n) {
+        if (n > this.slides.length) {
+            this.slideIndex = 1;
+        }
+        if (n < 1) {
+            this.slideIndex = this.slides.length;
+        }
+        // console.log(this.slideIndex);
+
+        try {
+            this.hanson.style.opacity = "0";
+
+            if (n === 3) {
+                this.hanson.classList.add("animated");
+                setTimeout(() => {
+                    this.hanson.style.opacity = "1";
+                    this.hanson.classList.add("slideInUp");
+                }, 3000);
+            } else {
+                this.hanson.classList.remove("slideInUp");
+            }
+        } catch (err) {}
+
+        this.slides.forEach((slide) => {
+            slide.style.display = "none";
+            slide.classList.remove("animated", "slideInUp");
+        });
+
+        this.slides[this.slideIndex - 1].style.display = "block";
+        this.slides[this.slideIndex - 1].classList.add("animated", "slideInUp");
+    }
+
+    plusSlides(n) {
+        this.showSlides((this.slideIndex += n));
+    }
+
+    render() {
+        try {
+            this.hanson = document.querySelector(".hanson");
+        } catch (err) {}
+
+        this.btns.forEach((btn) => {
+            btn.addEventListener("click", () => {
+                this.plusSlides(1);
+            });
+
+            btn.parentNode.previousElementSibling.addEventListener(
+                "click",
+                (e) => {
+                    e.preventDefault();
+                    this.slideIndex = 1;
+                    this.showSlides(this.slideIndex);
+                }
+            );
+        });
+
+        this.showSlides(this.slideIndex);
+    }
+}
