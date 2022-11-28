355121f68946efe05b5a241687aec0c3cf064ea0
Sviatoslav <blackdbf@gmail.com>
2022-11-28 14:34:20 +0200

    fix problems with second page. added (try & catch)

--- Project#3OOP/src/js/main.js
+++ Project#3OOP/src/js/main.js
@@ -2,6 +2,7 @@ import MainSlider from './modules/slider/slider-main';
 import MiniSlider from './modules/slider/slider-mini';
 import VideoPlayer from './modules/playVideo';
 import Difference from './modules/difference';
+import Form from './modules/form';
 
 window.addEventListener('DOMContentLoaded', () => {
     const slider = new MainSlider({ btns: '.next', container: '.page' });
@@ -40,4 +41,6 @@ window.addEventListener('DOMContentLoaded', () => {
     new Difference('.officerold', '.officernew', '.officer__card-item').init();
     // const difference = new Difference('.officerold', '.officernew', '.officer__card-item');
     // difference.init();
+
+    new Form('.form').init();
 });
