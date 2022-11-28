213608d1d64e6d67413e5348b1b7de8a8dc4533f
Sviatoslav <blackdbf@gmail.com>
2022-11-28 12:12:41 +0200

    added all sliders and difference cards

--- Project#3OOP/src/js/main.js
+++ Project#3OOP/src/js/main.js
@@ -1,10 +1,43 @@
 import MainSlider from './modules/slider/slider-main';
+import MiniSlider from './modules/slider/slider-mini';
 import VideoPlayer from './modules/playVideo';
+import Difference from './modules/difference';
 
 window.addEventListener('DOMContentLoaded', () => {
-    const slider = new MainSlider({ page: '.page', btns: '.next' });
+    const slider = new MainSlider({ btns: '.next', container: '.page' });
     slider.render();
+
+    const showUpSlider = new MiniSlider({
+        container: ".showup__content-slider",
+        prev: ".showup__prev",
+        next: ".showup__next",
+        activeClass: "card-active",
+        animate: true,
+    });
+    showUpSlider.init();
+
+    const modulesSlider = new MiniSlider({
+        container: ".modules__content-slider",
+        next: ".modules__info-btns .slick-next",
+        prev: ".modules__info-btns .slick-prev",
+        activeClass: "card-active",
+        animate: true,
+        autoplay: true,
+    });
+    modulesSlider.init();
+
+    const feedSlider = new MiniSlider({
+        container: ".feed__slider",
+        next: ".feed__slider .slick-next",
+        prev: ".feed__slider .slick-prev",
+        activeClass: "feed__item-active",
+    });
+    feedSlider.init();
     
     const player = new VideoPlayer('.showup .play', '.overlay');
     player.init();
+
+    new Difference('.officerold', '.officernew', '.officer__card-item').init();
+    // const difference = new Difference('.officerold', '.officernew', '.officer__card-item');
+    // difference.init();
 });
